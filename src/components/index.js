c22a102e2a63c4ce463bc04372c7d14bb56b220c
Akshay200228 <sankpla2812@gmail.com>
2023-07-13 18:42:05 +0530

    Done Task2

--- src/components/index.js
+++ src/components/index.js
@@ -3,19 +3,15 @@ import Footer from "./Footer";
 import Aboutus from "./Aboutus";
 import Hero1 from "./Hero1";
 import Status from "./Status";
-import ClientSlider from "./ClientSlider";
 import Community from "./Community";
 import Explore from "./Explore";
 import Testimonial from "./Testimonial";
-import CategoryForm from "./ContactForm";
 
 export {
   Navbar,
   Hero1,
   Status,
   Explore,
-  ClientSlider,
-  CategoryForm,
   Community,
   Aboutus,
   Testimonial,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
                 <Explore />
                 <Community />
                 <Testimonial />
-                {/* <Status /> */}
             </div>
         </div>
     )
